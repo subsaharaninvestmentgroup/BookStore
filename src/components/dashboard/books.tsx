1033d750730e4961516a3b5d2e557ddf0f2b4a21
Daniel Awuni <danielawuni53@gmail.com>
2025-09-03 20:28:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/dashboard/books.tsx
+++ src/components/dashboard/books.tsx
@@ -158,7 +158,7 @@ export default function Books({ onAddBook, onEditBook }: BooksProps) {
                 Export
                 </span>
               </Button>
-              <Link href="/store" legacyBehavior>
+              <Link href="/store">
                 <Button size="sm" variant="outline" className="h-8 gap-1 w-full sm:w-auto">
                   <Store className="h-3.5 w-3.5" />
                   <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
